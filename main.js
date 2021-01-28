fddb9238fb1e051a55fc54ba2a7cb9cab8ee4e84
kanishkar-star <71880783+kanishkar-star@users.noreply.github.com>
2021-01-28 08:28:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function set_text(){
+    input = document.getElementById("name_input").value;
+    if(input == ""){
+        alert("Please Enter your name");
+    }
+    else{
+        alert("Your name is added Successfully");
+    }
+
+}
